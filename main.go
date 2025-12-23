69fd48d31fb259ef97b83820ff8516658a42c01b
Gokul Suresh <50659225+xenomech@users.noreply.github.com>
2025-12-23 10:27:20 +0530

    feat: add basic git operations (#7)
    * feat: add basic git operations
    
    * feat: add git status for repositories

--- main.go
+++ main.go
@@ -3,12 +3,18 @@ package main
 import (
 	"fmt"
 	"portal/internal/config"
+	"portal/internal/git"
 )
 
 func main() {
 	fmt.Println("hello world")
+
+	// Test GetStatus with portal repository
+	status := git.GetStatus(`/Users/xenomech/_p/portal`)
+	fmt.Printf("Status: %+v\n", status)
+
 	cfg, err := config.Load()
-	fmt.Println(cfg.Repos)
+	// fmt.Println(cfg.Repos)
 	fmt.Println(err)
 	cfg.AddGitRepositoryToConfig(
 		"abc",
